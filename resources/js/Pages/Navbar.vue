d57cd5bbad632b1dbbbefa1902d7bb70da66275f
miripower <polmivi@gmail.com>
2025-01-10 17:14:41 +0100

    Creando el Navbar

---
+++ resources/js/Pages/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="bg-blue-600">
+        <div class="container mx-auto flex justify-between items-center">
+            <a href="/" class="text-white text-2xl font-bold">Alumni</a>
+            <ul class="flex">
+                <li class="ml-6">
+                    <a href="/" class="text-white">Home</a>
+                </li>
+                <li class="ml-6">
+                    <a href="/register" class="text-white">Register</a>
+                </li>
+                <li class="ml-6">
+                    <a href="/login" class="text-white">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
