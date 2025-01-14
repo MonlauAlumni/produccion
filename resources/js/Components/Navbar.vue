148f57aea5ca31af3e2daa899353395bcbf56481
miripower <polmivi@gmail.com>
2025-01-14 20:18:51 +0100

    small fix

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="bg-blue-800 p-4">
         <div class="container mx-auto flex justify-between items-center">
             <div class="flex gap-4 items-center">
-                <img src="/public/images/logo.png" alt="Alumni" class="w-16 h-16">
+                <img href="/" src="/public/images/logo.png" alt="Alumni" class="w-16 h-16">
                 <a href="/" class="text-white text-3xl font-bold">Alumni</a>
             </div>
             <ul class="flex">
