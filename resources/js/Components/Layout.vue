aaabb55f11bd70f1076154d744d280246432280a
lucasmaragu <lucasmaragu@gmail.com>
2025-03-06 11:00:55 -0600

    feat: new sidebar y header

--- resources/js/Components/Layout.vue
+++ resources/js/Components/Layout.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="min-h-screen bg-gray-50 flex flex-col">
-    <!-- Navbar (Header arriba) -->
-    <Navbar :auth="auth" />
+    <!-- Navbar (Header siempre arriba, fijo en la parte superior) -->
+    <Navbar :auth="auth" class="w-full" />
 
-    <div class="flex flex-1">
+    <!-- Main container for Sidebar and Content -->
+    <div class="flex flex-1"> 
       <!-- Sidebar (Izquierda, debajo del Navbar) -->
-      <Sidebar :username="auth?.user?.name || 'Usuario Alumni'" class="hidden md:block md:w-64" />
+      <Sidebar 
+        :username="auth?.user?.name || 'Usuario Alumni'" 
+        class="hidden md:block md:w-64 md:h-[calc(100vh-64px)]" 
+      />
 
       <!-- Main Content -->
       <main class="flex-1 p-6 bg-gray-50">
@@ -15,23 +19,21 @@
   </div>
 </template>
 
-  
-  <script>
-  import Navbar from '@/Components/Navbar.vue';
-  import Sidebar from '@/Components/Sidebar.vue';
-  
-  export default {
-    name: 'AlumniLayout',
-    components: {
-      Navbar,
-      Sidebar
-    },
-    props: {
-      auth: {
-        type: Object,
-        default: () => ({ user: null })
-      }
+<script>
+import Navbar from '@/Components/Navbar.vue';
+import Sidebar from '@/Components/Sidebar.vue';
+
+export default {
+  name: 'AlumniLayout',
+  components: {
+    Navbar,
+    Sidebar
+  },
+  props: {
+    auth: {
+      type: Object,
+      default: () => ({ user: null })
     }
-  };
-  </script>
-  
+  }
+};
+</script>
