e1a37bd4fc48f0ff7f6db8f3dffb895cb8e16169
lucasmaragu <lucasmaragu@gmail.com>
2025-05-15 15:58:19 +0000

    feat: register profile fix

--- app/Observers/ProfileObserver.php
+++ app/Observers/ProfileObserver.php
@@ -8,15 +8,17 @@ class ProfileObserver
     public function created(Profile $profile)
 {
 
-
-    if ($profile->user->hasRole('alumne')) {
+  
+ 
         $profile->slang = strtolower(
             str_replace(' ', '-', trim(
                 "{$profile->user->name}-{$profile->user->last_name_1}" .
                 ($profile->user->last_name_2 ? "-{$profile->user->last_name_2}" : "")
             ))
         );
-    }
+         $profile->save();
+   
+       
 }
 
 
